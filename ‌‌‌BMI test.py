e40aba1345a4d8be4cd780c1829930658ce11002
RezvaneRazavi <50709968+RezvaneRazavi@users.noreply.github.com>
2021-08-04 21:03:03 +0430

    Update ‌‌‌BMI test.py

--- ‌‌‌BMI test.py
+++ ‌‌‌BMI test.py
@@ -6,11 +6,11 @@ height = float(input("and height(m) for exampel-> 1.65:  "))
 BMI = (weight / (height * height))
 if BMI < 18.5:
     result = "Underweight!"
-elif BMI >= 18.5 and BMI <= 24.9:
+elif BMI >= 18.5 and BMI < 25:
     result = "Normal!"
-elif BMI >= 25 and BMI <= 29.9:
+elif BMI >= 25 and BMI < 30:
     result = "Overweight!"
-elif BMI >= 30 and BMI <= 34.9:
+elif BMI >= 30 and BMI < 35:
     result = "Obese!"
 elif 35 < BMI:
     result = "Extremely Obese!"
